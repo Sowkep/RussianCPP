e53f8590f2a2fac344a4f3c9cd0cefd2a5c26ef9
Sowkep <gogotasik@gmail.com>
2023-12-04 23:21:05 +0800

    Big update

--- defines.h
+++ defines.h
@@ -2,12 +2,10 @@
 //изначально всё указывается в мужском роде (если слово не изменяется по родма, то пишется как пишется)
 // (definesf и definesu для других родов)
 
+//ОСНОВЫ C++ (iostream и прочее)
+
 //импорты для работы
 #include "imports.h"
-//сортировка проще говоря
-#include "definesf.h"
-#include "definesu.h"
-#include "definesm.h"
 //типы данных
 #define целоечисло int
 #define булевыйтип bool
@@ -36,6 +34,38 @@
 #define остановить break
 #define продолжить continue
 #define вернуть return
+#define для for
+#define пока while
+#define сделать do
+#define проверитьцикл while
 //прочее
 #define идтик goto
+#define ничего NULL
 //остальное
+#define плюс +
+#define минус -
+#define умножить *
+#define разделить /
+#define остатокотделения %
+#define встепени **
+#define инкремент ++
+#define дикремент --
+#define больше >
+#define меньше <
+#define равно =
+
+//МАТЕМАТИКА (cmath)
+//все математические действия
+#define квадратныйкорень sqrt
+#define модуль abs
+#define косинус cos
+#define синус sin
+#define тангенс tan
+#define арккосинус acos
+#define арксинус asin
+#define арктангенс atan
+#define арктангенс2 atan2
+#define логарифм log
+#define бинарныйлогарифм log2
+
+//ВЕКТОРА (vector)
